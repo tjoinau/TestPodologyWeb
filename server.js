f7b88acdfb5e6579608293e7be55dc76ae34e43f
tjo <tjo@LAPTOP-121>
2023-01-27 14:00:23 +0100

    try

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/TestPodologyWeb'));
+app.use(express.static(__dirname + '/dist/test-podology-web'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/TestPodologyWeb/'));
+res.sendFile(path.join(__dirname+'/dist/test-podology-web/'));
 });
 
 // Start the app by listening on the default Heroku port
